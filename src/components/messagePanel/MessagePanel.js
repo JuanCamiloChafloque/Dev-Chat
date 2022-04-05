8ded95afa7c9918ad8e93ea96d36c57a357978f3
Camilo <camilochafloque@gmail.com>
2022-04-04 20:51:03 -0500

    Message Panel screens finished

--- src/components/messagePanel/MessagePanel.js
+++ src/components/messagePanel/MessagePanel.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Segment, Comment } from "semantic-ui-react";
+import MessageForm from "./MessageForm";
+import MessagesHeader from "./MessagesHeader";
 
 const MessagePanel = () => {
-  return <div>MessagePanel</div>;
+  return (
+    <>
+      <MessagesHeader />
+      <Segment>
+        <Comment.Group className="messages"></Comment.Group>
+      </Segment>
+      <MessageForm />
+    </>
+  );
 };
 
 export default MessagePanel;
